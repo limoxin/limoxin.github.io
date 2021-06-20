2d8f38be5dc2410911952e2fd3f932a393db94e0
limoxin <limoxin2017@outlook.com>
2021-06-20 18:37:11 +0800

    supermall

---
+++ js/chunk-2d21a3d2.3f4d88f1.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d21a3d2"],{bb51:function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},r=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("h2",[e._v("分类")])])}],a={name:"category"},u=a,l=t("2877"),s=Object(l["a"])(u,c,r,!1,null,null,null);n["default"]=s.exports}}]);
+//# sourceMappingURL=chunk-2d21a3d2.3f4d88f1.js.map
